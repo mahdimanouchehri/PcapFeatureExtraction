427d8d583de8613d8e96fa105ef3eba5f21b1530
mahdimanouchehri <130359545+mahdimanouchehri@users.noreply.github.com>
2024-08-28 03:00:15 -0700

    Update flow_session.py

--- flow_session.py
+++ flow_session.py
@@ -1,49 +1,42 @@
-import csv
-from collections import defaultdict
-
-from scapy.sessions import DefaultSession
-
-from .features.context.packet_direction import PacketDirection
-from .features.context.packet_flow_key import get_packet_flow_key
-from .flow import Flow
+from scapy.all import rdpcap
+from scapy.sendrecv import sniff
+from features.context.packet_direction import PacketDirection
+from features.context.packet_flow_key import get_packet_flow_key
+from flow import Flow
+from tqdm import tqdm
 
 EXPIRED_UPDATE = 40
-MACHINE_LEARNING_API = "http://localhost:8000/predict"
-GARBAGE_COLLECT_PACKETS = 100
+class FlowSession:
 
-
-class FlowSession(DefaultSession):
     """Creates a list of network flows."""
+    def __init__(self, pcap_link):
 
-    def __init__(self, *args, **kwargs):
-        self.flows = {}
-        self.csv_line = 0
-
-        if self.output_mode == "flow":
-            output = open(self.output_file, "w")
-            self.csv_writer = csv.writer(output)
-
+        self.pcap_link = pcap_link
+        self.packets = self.read_pcap()
         self.packets_count = 0
+        self.flows = {}
 
-        self.clumped_flows_per_label = defaultdict(list)
+    def create_flows(self):
+        for packet in tqdm(self.packets):
+            self.on_packet_received(packet)
 
-        super(FlowSession, self).__init__(*args, **kwargs)
+    def read_pcap(self):
+        #packets = sniff(offline=self.pcap_link)
+        packets = rdpcap(self.pcap_link) # alternative way for read pcap file
+        return packets
 
-    def toPacketList(self):
-        # Sniffer finished all the packets it needed to sniff.
-        # It is not a good place for this, we need to somehow define a finish signal for AsyncSniffer
-        self.garbage_collect(None)
-        return super(FlowSession, self).toPacketList()
+    def get_flows(self) -> list:
+        return self.flows.values()
 
     def on_packet_received(self, packet):
         count = 0
         direction = PacketDirection.FORWARD
 
-        if self.output_mode != "flow":
-            if "TCP" not in packet:
-                return
-            elif "UDP" not in packet:
-                return
+        # if "TCP" not in packet:
+        #     return
+        #
+        # elif "UDP" not in packet:
+        #     return
 
         try:
             # Creates a key variable to check
@@ -81,58 +74,14 @@ class FlowSession(DefaultSession):
                     flow = Flow(packet, direction)
                     self.flows[(packet_flow_key, count)] = flow
                     break
+
         elif "F" in str(packet.flags):
             # If it has FIN flag then early collect flow and continue
             flow.add_packet(packet, direction)
-            self.garbage_collect(packet.time)
+            #self.garbage_collect(packet.time)
             return
 
         flow.add_packet(packet, direction)
-
-        if not self.url_model:
-            GARBAGE_COLLECT_PACKETS = 10000
-
-        if self.packets_count % GARBAGE_COLLECT_PACKETS == 0 or (
-            flow.duration > 120 and self.output_mode == "flow"
-        ):
-            self.garbage_collect(packet.time)
-
-    def get_flows(self) -> list:
-        return self.flows.values()
-
-    def garbage_collect(self, latest_time) -> None:
-        # TODO: Garbage Collection / Feature Extraction should have a separate thread
-        if not self.url_model:
-            print("Garbage Collection Began. Flows = {}".format(len(self.flows)))
-        keys = list(self.flows.keys())
-        for k in keys:
-            flow = self.flows.get(k)
-
-            if (
-                latest_time is None
-                or latest_time - flow.latest_timestamp > EXPIRED_UPDATE
-                or flow.duration > 90
-            ):
-                data = flow.get_data()
-
-                if self.csv_line == 0:
-                    self.csv_writer.writerow(data.keys())
-
-                self.csv_writer.writerow(data.values())
-                self.csv_line += 1
-
-                del self.flows[k]
-        if not self.url_model:
-            print("Garbage Collection Finished. Flows = {}".format(len(self.flows)))
+    #    print("flow:", flow)
 
 
-def generate_session_class(output_mode, output_file, url_model):
-    return type(
-        "NewFlowSession",
-        (FlowSession,),
-        {
-            "output_mode": output_mode,
-            "output_file": output_file,
-            "url_model": url_model,
-        },
-    )
